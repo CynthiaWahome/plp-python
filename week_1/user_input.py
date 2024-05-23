fac1a0df23f455f4c5332a17b262248701e0d239
cynthia <cynthiaawsajira@gmail.com>
2024-05-23 15:39:50 +0300

    first commit

---
+++ week_1/user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+
+print("Hello", name,"!\nYou are", age, "years old and you live in", location)
