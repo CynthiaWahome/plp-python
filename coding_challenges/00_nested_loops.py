2e7ffe5ba107cc2339235d79332ea1d94272f754
cynthia <cynthiaawsajira@gmail.com>
2024-08-20 01:47:31 +0300

    [chore] updated the solutions to coding challenge 00 and 01

--- coding_challenges/00_nested_loops.py
+++ coding_challenges/00_nested_loops.py
@@ -1,4 +1,7 @@
-## Nested Loops
+#!/usr/bin/env python3
+
+"""
+Nested Loops
 
 Consider a doctor's office schedule. Each appointment is 30 minutes long. A program to print available appointments can use a nested for loop where:
 
@@ -11,3 +14,14 @@ In this example:
 
 - The outer loop iterates over the time's hour portion between 8 and 9.
 - The inner loop iterates over the time's minute portion between 0 and 59.
+"""
+
+hour = 8
+minute = 0
+
+while hour < 10:
+    while minute < 59:
+        print(hour, ":", minute)
+        minute +=30
+    hour += 1
+    minute = 0
