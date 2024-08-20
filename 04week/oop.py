974102aa94265c4c81118fe0957c286a5e892151
cynthia <cynthiaawsajira@gmail.com>
2024-08-20 21:24:05 +0300

    [chore] update 04week

---
+++ 04week/oop.py
@@ -0,0 +1,15 @@
+"""
+Create a Python class named `Person`.  
+The `Person` class should have the following attributes:  
+- `name`: representing the person's name.  
+- `age`: representing the person's age.  
+- `gender`: representing the person's gender.  
+
+Implement a method called `introduce` that prints a message introducing the person with their name, age, and gender.  
+
+Create an instance of the `Person` class and call the `introduce` method to display the person's information.  
+
+Create a GitHub repository for your assignment and submit the link.
+
+
+"""
